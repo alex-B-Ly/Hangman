90557e597206224dd630f0daebe787208b181ada
Alex Ly <alexly@Alexs-MBP.home>
2016-01-25 16:57:33 -0500

    Started creating game object with startGame method.

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+// file REQ
+var prompt = require('prompt');
 var Word = require('./Word.js');
 
+// Start Prompt
+prompt.start();
+
+// Game object
+var game = {
+  wordBank: ['anaphylaxis', 'mitochondrion', 'opossum'],
+  guessesRemaining: 10,
+  currentWrd: null,
+
+  startGame: function(wrd){
+    currentWrd = new Word(this.wordBank[Math.floor(Math.random() * this.wordBank.length)]);
+    currentWrd.getLets();
+    // TODO Remove console log below when done testing.
+    console.log(currentWrd);
+  }
+}
+
+game.startGame();
+
 // cat test
-var cat = new Word('cat');
-cat.getLets();
-cat.checkIfLetterFound('a');
-cat.wordRender();
+// var cat = new Word('cat');
+// cat.getLets();
+// cat.checkIfLetterFound('a');
+// cat.wordRender();
